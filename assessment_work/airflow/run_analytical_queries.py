26a16a127f7952a447dade00d5cc589c35672d4c
Vladyslav Babenko <big.ben@no-apps.team>
2026-02-09 10:05:38 +0200

    fix query

--- assessment_work/airflow/run_analytical_queries.py
+++ assessment_work/airflow/run_analytical_queries.py
@@ -21,7 +21,7 @@ with DAG(
     tags=['bi', 'analysis', 'redshift'],
 ) as dag:
 
-    # Запит 1: Топ-3 штати за продажами ТБ серед молоді
+    # Запит 1: В якому штаті було куплено найбільше телевізорів покупцями від 20 до 30 років за першу декаду вересня?
     tv_brand_analysis = RedshiftDataOperator(
         task_id='tv_brand_analysis_20_30_age',
         workgroup_name=WORKGROUP_NAME,
@@ -29,21 +29,33 @@ with DAG(
         sql="""
             DROP TABLE IF EXISTS gold.tv_analysis_result;
             CREATE TABLE gold.tv_analysis_result AS
-            WITH young_customers AS (
-                SELECT client_id, state, 
-                       DATEDIFF(year, birth_date, GETDATE()) as age
-                FROM gold.user_profiles_enriched
-                WHERE birth_date IS NOT NULL 
-                AND state IS NOT NULL AND state != ''
+            WITH base AS (
+                SELECT
+                    s.client_id,
+                    yc.state,
+                    LOWER(TRIM(s.product_name)) AS product_name_norm,
+                    CAST(s.purchase_date AS DATE) AS purchase_date,
+                    CAST(yc.birth_date AS DATE) AS birth_date
+                FROM silver_ext.sales s
+                JOIN gold.user_profiles_enriched yc
+                    ON s.client_id = yc.client_id
+                WHERE yc.birth_date IS NOT NULL
+                AND yc.state IS NOT NULL
+                AND yc.state <> ''
+            ),
+            state_sales AS (
+                SELECT
+                    state,
+                    COUNT(*) AS sales_count
+                FROM base
+                WHERE product_name_norm = 'tv'
+                AND purchase_date BETWEEN DATE '2022-09-01' AND DATE '2022-09-10'
+                AND DATEDIFF(year, birth_date, purchase_date) BETWEEN 20 AND 30
+                GROUP BY state
             )
-            SELECT s.product_name, yc.state, COUNT(*) as sales_count
-            FROM silver_ext.sales s
-            JOIN young_customers yc ON s.client_id = yc.client_id
-            WHERE yc.age BETWEEN 20 AND 30
-            AND s.product_name = 'TV'
-            GROUP BY 1, 2
-            ORDER BY sales_count DESC
-            LIMIT 3;
+            SELECT *
+            FROM state_sales
+            WHERE sales_count = (SELECT MAX(sales_count) FROM state_sales);
         """,
         wait_for_completion=True
     )
